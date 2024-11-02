0a0f813611d4c389cf2539624ac2c406bf34ded1
Omar_Gihad <101315332+Omar-Gihad@users.noreply.github.com>
2024-11-02 21:06:05 +0200

    Update apiHandler.js

--- src/utils/apiHandler.js
+++ src/utils/apiHandler.js
@@ -1,22 +1,14 @@
-// import axios from "axios";
-
-// export const Axios = axios.create({
-//   baseURL: "https://droob.medicalvisionarabia.com/api",
-// <<<<<<< HEAD
-//   headers:{
-//     "Accept-Language":"ar",
-// =======
-//   headers: {
-//     "Accept-Language": "ar",
-// >>>>>>> 8c7c1b7d47ee53d3cab1cc5dc191b2dba49cafb4
-//   },
-// });
-
 import axios from "axios";
 
 export const Axios = axios.create({
   baseURL: "https://droob.medicalvisionarabia.com/api",
+<<<<<<< HEAD
+  headers:{
+    "Accept-Language":"ar",
+=======
   headers: {
     "Accept-Language": "ar",
+>>>>>>> 8c7c1b7d47ee53d3cab1cc5dc191b2dba49cafb4
   },
 });
+
